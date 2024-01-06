49a2c2b161351bbdcc052cb8571e74bff7bd80cd
Igor Nunes <inunes.us@gmail.com>
2024-01-06 01:38:34 -0300

    feat: add Developer class

---
+++ GerenciadorDeTarefas/Models/Users/Developer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeTarefas.Models.Users
+{
+    internal class Developer : User
+    {
+        public Developer(string name, string email, string password) : base(name, email, password)
+        {
+        }
+    }
+}
