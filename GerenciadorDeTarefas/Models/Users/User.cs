bf6608810873a4dbccc3a6e126b5fb9870332aa8
Igor Nunes <inunes.us@gmail.com>
2024-01-06 01:05:37 -0300

    feat: add User class

---
+++ GerenciadorDeTarefas/Models/Users/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeTarefas.Models.Users
+{
+    public abstract class User
+    {
+        private string _name;
+        private string _email;
+        private string _password;
+
+        public string Name { get { return _name; } }
+        public string Email { get { return _email; } }
+        public string Password { get { return _password; } }
+
+        public User(string name, string email, string password) 
+        {
+            this._name = name;
+            this._email = email;
+            this._password = password;
+        }
+
+        public void UpdatePassword(string password)
+        {
+            _password = password;
+        }
+
+        public void UpdateEmail(string email)
+        {
+            _email = email;
+        }
+
+        public void UpdateName(string name)
+        {
+            _name = name;
+        }
+
+    }
+}
