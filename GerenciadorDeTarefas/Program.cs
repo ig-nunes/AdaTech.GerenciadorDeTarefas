47a49873f5802492b96769e1f34199a3469f467c
Igor Nunes <inunes.us@gmail.com>
2024-01-07 01:26:45 -0300

    feat: login menu in Program

--- GerenciadorDeTarefas/Program.cs
+++ GerenciadorDeTarefas/Program.cs
@@ -1,5 +1,7 @@
 ﻿using GerenciadorDeTarefas.Models.Users;
 using GerenciadorDeTarefas.Repositories;
+using GerenciadorDeTarefas.Authentication;
+using GerenciadorDeTarefas.Utils;
 
 namespace GerenciadorDeTarefas
 {
@@ -12,24 +14,117 @@ namespace GerenciadorDeTarefas
 
             Console.WriteLine("Bem-vindo ao Sistema de Gerenciamento de Tarefas!");
 
-            Console.Write("Digite seu email: ");
-            string email = Console.ReadLine();
-
-            Console.Write("Digite sua senha: ");
-            string password = Console.ReadLine();
-
-
-            User authenticatedUser = UserManager.AuthenticateUser(email, password);
-
-
-            if (authenticatedUser != null)
+            while (true)
             {
-                Console.WriteLine($"Usuário autenticado com sucesso. Usuario: {authenticatedUser.GetType()}");
+                Console.Write($"O que você deseja fazer?\n" +
+                    $"  1 - Fazer Login\n" +
+                    $"  2 - Cadastrar novo usuário;\n" +
+                    $"  3 - Sair\n");
+                string? escolha = Console.ReadLine();
+
+                switch (escolha)
+                {
+                    case "1":
+                        var authenticatedUser = Authentication.Authentication.Login();
+
+                        if (authenticatedUser != null)
+                        {
+                            Console.Clear();
+                            authenticatedUser.GetAccessStrategy().Access(authenticatedUser);
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Ocorreu algum erro ao fazer o login. Retornando para o menu...");
+                        }
+
+                        break;
+                    case "2":
+                        Register();
+                        Console.WriteLine("Usuário Criado com sucesso!");
+                        Console.ReadKey();
+                        break;
+                    case "3":
+                        Console.WriteLine("Saindo do sistema...");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Erro: não foi possível identificar.");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+
             }
-            else
+
+        }
+
+        private static void Register()
+        {
+            while (true)
             {
-                Console.WriteLine("Falha na autenticação. Email ou senha incorretos.");
+                User? user;
+                Console.Write("Digite seu nome: ");
+                string name = Console.ReadLine()!;
+
+                Console.Write("Digite seu email: ");
+                string email = Console.ReadLine()!;
+
+                Console.Write("Digite uma senha: ");
+                string password = Console.ReadLine()!;
+
+                UserType? userType = null;
+
+                var continuar = true;
+
+                while (continuar)
+                {
+                    Console.Write($"Escolha o tipo de usuário:\n" +
+                        $"  1 - Tech Leader;\n" +
+                        $"  2 - Developer;\n");
+                    string type = Console.ReadLine()!;
+
+                    switch(type)
+                    {
+                        case "1":
+                            userType = UserType.TechLeader;
+                            continuar = false;
+                            break;
+                        case "2":
+                            userType = UserType.Developer;
+                            continuar = false;
+                            break;
+                        default:
+                            Console.WriteLine("Erro: Valor não válido.");
+                            Console.ReadKey();
+                            break;
+                    }
+                    Console.Clear();
+                }
+
+                Console.WriteLine($"Usuário:\n" +
+                    $"Nome: {name}\n" +
+                    $"E-mail: {email}\n" +
+                    $"Password: {password}" +
+                    $"Tipo: {userType.ToString()}");
+
+                Console.ReadKey();
+
+                string hashPassword = HashClass.SetHashPassword(password);
+
+                if (userType == UserType.TechLeader)
+                {
+                    user = new TechLeader(name, email, hashPassword);
+                    UserManager.SaveUser(user);
+                }
+                else if (userType == UserType.Developer)
+                {
+                    user = new Developer(name, email, hashPassword);
+                    UserManager.SaveUser(user);
+                }
+                break;
             }
+            
         }
     }
 }
