063a073af36c81bd122fba655007eb18f9345c95
Igor Nunes <inunes.us@gmail.com>
2024-01-07 01:23:04 -0300

    feat: add Authetication class;

---
+++ GerenciadorDeTarefas/Authentication/Authentication.cs
@@ -0,0 +1,37 @@
+﻿using GerenciadorDeTarefas.Models.Users;
+using GerenciadorDeTarefas.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeTarefas.Authentication
+{
+    public static class Authentication
+    {
+        public static User? Login()
+        {
+            Console.Write("Digite seu email: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Digite sua senha: ");
+            string password = Console.ReadLine();
+
+
+            User authenticatedUser = UserManager.AuthenticateUser(email, password);
+
+
+            if (authenticatedUser != null)
+            {
+                Console.WriteLine($"Usuário autenticado com sucesso!");
+                return authenticatedUser;
+            }
+            else
+            {
+                Console.WriteLine("Falha na autenticação. Email ou senha incorretos.");
+                return null;
+            }
+        }
+    }
+}
