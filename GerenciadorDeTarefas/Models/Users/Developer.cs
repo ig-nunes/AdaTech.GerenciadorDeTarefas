34e63376a87d5b09973f700801bbbdacc0894d13
Igor Nunes <inunes.us@gmail.com>
2024-01-08 16:12:25 -0300

    fix: change class access modifier to public

--- GerenciadorDeTarefas/Models/Users/Developer.cs
+++ GerenciadorDeTarefas/Models/Users/Developer.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace GerenciadorDeTarefas.Models.Users
 {
-    internal class Developer : User
+    public class Developer : User
     {
         public override UserType UserType => UserType.Developer;
         public Developer(string name, string email, string password) : base(name, email, password)
