215b691dd97d72454cf4158ad30ea11334f3001f
Igor Nunes <inunes.us@gmail.com>
2024-01-10 11:45:46 -0300

    refactor: add Messages class

---
+++ GerenciadorDeTarefas/Utils/Messages.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeTarefas.Utils
+{
+    public class Messages
+    {
+        public static void PressAnyKeyToContinue()
+        {
+            Console.WriteLine("Aperte qualquer botão para continuar.");
+            Console.ReadKey();
+        }
+
+        public static void PressAnyKeyToContinue(string message)
+        {
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+
+        public static void ExitMenu(string message) 
+        {
+            Console.WriteLine(message);
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+
+    }
+}
